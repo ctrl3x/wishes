fe54af13efe1a1d106e2e83f7a5b4f42aba87b3b
Elizaveta Konopyova <konoplyova.liza18@yandex.ru>
2026-06-03 02:18:32 +0300

    about page

---
+++ v3/js/about-scroll.js
@@ -0,0 +1,22 @@
+(function () {
+  var target = 'about-content.html';
+  var navigated = false;
+
+  function goNext() {
+    if (navigated) return;
+    navigated = true;
+    window.location.href = target;
+  }
+
+  window.addEventListener('wheel', function (event) {
+    if (event.deltaY > 0) {
+      goNext();
+    }
+  }, { passive: true });
+
+  window.addEventListener('keydown', function (event) {
+    if (event.key === 'ArrowDown' || event.key === 'PageDown' || event.key === ' ') {
+      goNext();
+    }
+  });
+})();
